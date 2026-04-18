54f341d6685a9436bcfce904455710b0079449b2
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-04-18 16:24:22 +0200

    Skapat server med anslutning till databasfil och Middleware med CORS och inläsning av JSON

--- labb2_backend/server.js
+++ labb2_backend/server.js
@@ -1,8 +1,36 @@
 
-const createtable = require("./tables");
+////// SERVER /////
+
+//importerar Express
+const express = require("express");
+//Importerar CORS
+const cors = require("cors");
+//Importerar funktion som skapar databastabell
+const createTable = require("./tables");
+
+//Hämtar data/variabler från .env-fil (ex: PORT och DATABAS_URL)
+require("dotenv").config();
 
 
+//Skapar express applikation
+const app = express();
 
+//CORS - Tillåter requests från andra domäner
+app.use(cors());
+//Gör att server kan läsa JSON data
+app.use(express.json());
+
+///// DATABAS - via db.js och tables.js ////
 
 //Kör funktion som skapar tabell
 createTable();
+
+
+/////// STARTA SERVER /////
+
+const PORT = process.env.PORT;
+
+// Startar servern på en port från .env
+app.listen(PORT, () => {
+    console.log("server startad på port" + "" + PORT);
+});
